7847d0c425ea561c9fee66b554421cf253e3d551
noorunnisa7 <noorunnisa560@gmail.com>
2025-05-06 17:59:32 +0500

    Shopping cart

--- Class_18/add_to_cart.php
+++ Class_18/add_to_cart.php
@@ -7,15 +7,28 @@ $product_id = $_POST['product_id'];
 $qty = $_POST['qty'];
 $session_id = session_id();
 
-// echo "Product id: ".$product_id."<br> QTY: ".$qty;
 
-// echo "<br>";
+$select = "SELECT * from cart where product_id = $product_id And session_id = '$session_id'";
+$selectRun = mysqli_query($con,$select);
 
-// echo $session_id;
+$countRow = mysqli_num_rows($selectRun);
+
+echo $countRow;
+
+if($countRow > 0){
+    $update = "UPDATE cart set 
+               qty = qty +  $qty
+               where product_id = $product_id And session_id = '$session_id'";
+    $updateRun = mysqli_query($con,$update);
+}
+else{
+    $query = "INSERT into cart (product_id,session_id,qty) VALUES ($product_id,'$session_id',$qty)";
+    $queryRun = mysqli_query($con , $query);
+}
 
 
-$query = "INSERT into cart (product_id,session_id,qty) VALUES ($product_id,'$session_id',$qty)";
-$queryRun = mysqli_query($con , $query);
+
+
 
 header("location:cart.php");
 ?>
--- Class_18/cart.php
+++ Class_18/cart.php
@@ -17,23 +17,65 @@
             <h1>CART</h1>
 
             <div class="col-12">
+            <table class="table table-info table-striped">
+                        <thead>
+                           <tr>
+                           <th>S.No</th>
+                            <th>product title</th>
+                            <th>Product Price</th>
+                            <th>Product Qty</th>
+                            <th>Total</th> 
+                           </tr>                           
+                        </thead>
+                        <tbody>
                 <?php
                 $query = "SELECT * FROM cart c 
                         inner join products p  ON p.id = c.product_id
                         where c.session_id = '$session_id'";
                 $queryRUN = mysqli_query($con , $query);
+                $grandTotal = 0;
                 while($fetch = mysqli_fetch_assoc($queryRUN)){
-                    echo "Product ID: ".$fetch['product_id']."
-                    <br> QTY : ".$fetch['qty']."
-                    <br> SESSION : ".$fetch['session_id']."
-                    ";
-                    echo "<br>Product Name: ".$fetch['name'];
-                    echo "<br><img src='images/".$fetch['image']."' width='50px'>";
+
+                    ?>
+                            <tr>
+                                <td>1</td>
+                                <td><?php echo $fetch['name'];?>
+                                    <img src="images/<?php echo $fetch['image'];?>" width="50px">
+                                </td>
+                                <td><?php echo $fetch['price'];?></td>
+                                <td><?php echo $fetch['qty'];?></td>
+                                <td><?php echo $fetch['qty']* $fetch['price'];?></td>
+                            </tr>
+                        
+                    <?php
+                      $grandTotal += $fetch['qty'] * $fetch['price'];
+                    
                 }
                 
                 ?>
+
+                <tr>
+                    <td colspan="3"></td>
+                    <td>Grand Total</td>
+                    <td><?php echo $grandTotal;?></td>
+                </tr>
+                </tbody>
+                </table>
             </div>
         </div>
     </section>
 </body>
 </html>
+
+<?php
+$array = [1,2,3,10,20];
+
+$sum = 0;
+foreach($array as $value){
+    $sum +=  $value;
+    echo ' '.$sum;
+}
+
+
+
+?>
